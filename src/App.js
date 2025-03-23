f204dcaa7f39e70283f8adce0d561c3716f44709
Hyeokju Lee <dlhjwwe@naver.com>
2025-03-23 19:34:32 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const [idx, setIdx] = useState(0);
+  const onChange = (event) => setUsd(event.target.value);
+  const onSelect = (event) => setIdx(event.target.value);
 
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelect}>
+            {coins.map((coin, index) => (
+              <option value={index} key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>
+            <label htmlFor="usd">USD</label>
+            <input value={usd} id="usd" type="number" onChange={onChange} />
+          </div>
+          <div>
+            <label htmlFor={coins[idx].id}>{coins[idx].symbol}</label>
+            <input
+              value={usd / coins[idx].quotes.USD.price}
+              id={coins[idx].id}
+              type="number"
+              disabled
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
