0b526362be8bf13c4f4fa0e216362a6136700e03
Hyeokju Lee <dlhjwwe@naver.com>
2025-03-24 13:01:38 +0900

    #7.6 Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+      setLoading(false);
+    };
+
+    getMovie();
+  }, [id]);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Movie
+          id={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Detail;
